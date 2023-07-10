29f8fc3994a638e69c9c496f09df6a9ca1e274ca
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2023-07-10 10:11:14 +0200

    Adding a composable function to the mobile action interface

--- src/jvmMain/kotlin/mobile/actions/MobileAction.kt
+++ src/jvmMain/kotlin/mobile/actions/MobileAction.kt
@@ -1,9 +1,13 @@
 package mobile.actions
 
+import androidx.compose.runtime.Composable
 import mobile.MobileDevice
 
 interface MobileAction {
     val displayText: String
 
     fun executeAction(mobileDevice: MobileDevice)
+
+    @Composable
+    fun viewContribution()
 }
--- src/jvmMain/kotlin/mobile/actions/OpenScreenShareMobileAction.kt
+++ src/jvmMain/kotlin/mobile/actions/OpenScreenShareMobileAction.kt
@@ -1,5 +1,6 @@
 package mobile.actions
 
+import androidx.compose.runtime.Composable
 import mobile.MobileDevice
 
 class OpenScreenShareMobileAction : MobileAction {
@@ -8,5 +9,10 @@ class OpenScreenShareMobileAction : MobileAction {
 
     override fun executeAction(mobileDevice: MobileDevice) {
         Runtime.getRuntime().exec("scrcpy --serial ${mobileDevice.deviceName} --window-title=\"Toolcat Screen Cast (${mobileDevice.deviceName})\"")
+    }
+
+    @Composable
+    override fun viewContribution() {
+
     }
 }
