595326269bf584c373423f2f385c4cb82307f3e5
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2023-07-25 16:25:28 +0200

    Adding a package for Mobile Hardware

---
+++ src/jvmMain/kotlin/mobile/hardware/BatteryHardwareInfo.kt
@@ -0,0 +1,11 @@
+package mobile.hardware
+
+import mobile.MobileDevice
+
+fun MobileDevice.getBatteryLevel(): Float {
+    return 0F
+}
+
+fun MobileDevice.getBatteryHealth(): Float {
+    return 0F
+}
