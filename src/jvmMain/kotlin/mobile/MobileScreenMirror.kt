87db5d7de5d7ef73236fbf414069c043a4243391
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2023-07-26 16:43:22 +0200

    Fixed Screen Mirroring on MacOS

--- src/jvmMain/kotlin/mobile/MobileScreenMirror.kt
+++ src/jvmMain/kotlin/mobile/MobileScreenMirror.kt
@@ -1,9 +1,13 @@
 package mobile
 
+import kotlinx.coroutines.runBlocking
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun MobileDevice.openScreenMirror() {
     when (this.deviceType) {
         DeviceType.ANDROID -> {
-            Runtime.getRuntime().exec("scrcpy --serial ${this.serial} --window-title=\"Toolcat Screen Mirror (${this.serial})\"")
+            Runtime.getRuntime().exec("scrcpy --serial $serial")
         }
 
         DeviceType.IOS -> return
