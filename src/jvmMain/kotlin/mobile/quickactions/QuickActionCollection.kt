ded28418377a9adadd471b826eb92ef8116c832e
Anthony Hofmeister <contact@anthonyhfm.dev>
2023-07-27 18:02:04 +0200

    Screenshot Quick Action with Copying to Clipboard

--- src/jvmMain/kotlin/mobile/quickactions/QuickActionCollection.kt
+++ src/jvmMain/kotlin/mobile/quickactions/QuickActionCollection.kt
@@ -1,9 +1,13 @@
 package mobile.quickactions
 
+import mobile.quickactions.actions.OpenDeepLinkAction
+import mobile.quickactions.actions.ScreenshotAction
 import mobile.quickactions.actions.ToggleBluetoothAction
 import mobile.quickactions.actions.ToggleWifiAction
 
 val quickActionList: Array<QuickAction> = arrayOf(
     ToggleWifiAction(),
-    ToggleBluetoothAction()
+    ToggleBluetoothAction(),
+    ScreenshotAction(),
+    OpenDeepLinkAction()
 )
