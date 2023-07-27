76ca2ed9cf491dedc3d0f3dbfc8b2b32f4a2a2ae
Anthony Hofmeister <contact@anthonyhfm.dev>
2023-07-27 16:18:45 +0200

    Added some elements to the About View

--- src/jvmMain/kotlin/ui/views/NavAboutView.kt
+++ src/jvmMain/kotlin/ui/views/NavAboutView.kt
@@ -1,30 +1,68 @@
 package ui.views
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ui.Inter
 
 @Composable
 fun AboutView() {
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize(),
-
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .fillMaxSize()
     ) {
-        Image(
-            painter = painterResource("icons/toolcat-logo.png"),
-            contentDescription = null,
+        Column(
             modifier = Modifier
-                .height(200.dp)
-        )
+                .fillMaxSize(),
+
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource("icons/toolcat-logo.png"),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = "Toolcat",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Light,
+                letterSpacing = 4.sp,
+                fontFamily = Inter
+            )
+
+            Text(
+                text = "Made by Anthony Hofmeister",
+                fontWeight = FontWeight.Light,
+                fontFamily = Inter
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .align(Alignment.BottomCenter),
+
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Version 1.0.0",
+                fontWeight = FontWeight.Light,
+                fontFamily = Inter
+            )
+        }
     }
 }
